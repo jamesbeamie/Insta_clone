f9ef9ca74cebffc06ceace2f5f7b194a38d9359d
James <admin@James.local>
2019-11-11 10:51:21 +0300

    Initial commit

---
+++ MaPicha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaPicha
+//
+//  Created by James on 11/11/19.
+//  Copyright © 2019 James. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Oyah!, Oyah!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
